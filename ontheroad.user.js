6a608b24e37f22863e1278fd174003030bc8c0c0
Hurqalia <Hurqalia@users.noreply.github.com>
2016-04-23 18:12:05 +0200

    Update ontheroad.user.js

--- ontheroad.user.js
+++ ontheroad.user.js
@@ -4,7 +4,7 @@
 // @category       Layer
 // @version        0.0.1.20160423.001
 // @namespace      https://github.com/Hurqalia/ontheroad
-// @updateURL      https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
+// @updateURL      https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.meta.js
 // @downloadURL    https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
 // @description    [ontheroad-2016-04-23-000001] OnTheRoad
 // @include        https://www.ingress.com/intel*
