e10848fd09732cba76215deb56754dda06eb70ce
Hurqalia <Hurqalia@users.noreply.github.com>
2016-04-23 18:12:20 +0200

    Create ontheroad.meta.js

---
+++ ontheroad.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             ontheroad
+// @name           IITC plugin: OnTheRoad
+// @category       Layer
+// @version        0.0.1.20160423.001
+// @namespace      https://github.com/Hurqalia/ontheroad
+// @updateURL      https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.meta.js
+// @downloadURL    https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
+// @description    [ontheroad-2016-04-23-000001] OnTheRoad
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
