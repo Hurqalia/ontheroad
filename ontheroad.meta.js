db8904e1669ec33f335f76f77e021000a48109ac
Hurqalia <Hurqalia@users.noreply.github.com>
2016-05-06 17:08:10 +0200

    Update ontheroad.meta.js

--- ontheroad.meta.js
+++ ontheroad.meta.js
@@ -2,11 +2,12 @@
 // @id             ontheroad
 // @name           IITC plugin: OnTheRoad
 // @category       Layer
-// @version        0.0.2.20160423.001
-// @namespace      https://github.com/Hurqalia/ontheroad
+// @version        0.0.3.20160423.002
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.meta.js
 // @downloadURL    https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
-// @description    [ontheroad-2016-04-23-001] OnTheRoad
+// @installURL     https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
+// @description    [ontheroad-2016-04-23-002] OnTheRoad
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
