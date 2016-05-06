814f8e9e4318ff1a8258820a871743f373086a95
Hurqalia <Hurqalia@users.noreply.github.com>
2016-05-06 16:20:06 +0200

    Update ontheroad.meta.js

--- ontheroad.meta.js
+++ ontheroad.meta.js
@@ -2,11 +2,11 @@
 // @id             ontheroad
 // @name           IITC plugin: OnTheRoad
 // @category       Layer
-// @version        0.0.1.20160423.001
+// @version        0.0.2.20160423.001
 // @namespace      https://github.com/Hurqalia/ontheroad
 // @updateURL      https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.meta.js
 // @downloadURL    https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
-// @description    [ontheroad-2016-04-23-000001] OnTheRoad
+// @description    [ontheroad-2016-04-23-001] OnTheRoad
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
