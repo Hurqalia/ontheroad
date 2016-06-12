962d326a49498b4a14cc09fdfc98cc3b68f48b98
Hurqalia <Hurqalia@users.noreply.github.com>
2016-06-12 21:10:49 +0200

    Update ontheroad.meta.js
    @Jormund patch - Add folder support

--- ontheroad.meta.js
+++ ontheroad.meta.js
@@ -2,12 +2,12 @@
 // @id             ontheroad
 // @name           IITC plugin: OnTheRoad
 // @category       Layer
-// @version        0.1.0.20160423.005
+// @version        0.1.0.20160612.001
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.meta.js
 // @downloadURL    https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
 // @installURL     https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
-// @description    [ontheroad-2016-04-23-005] OnTheRoad
+// @description    [ontheroad-2016-06-12-001] OnTheRoad
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
