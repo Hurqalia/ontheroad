cef0991aca44f21b7a82054b27bd5e0f0de87cfd
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:27:44 +0200

    Update ontheroad.user.js
    Fix Niantics redirect

--- ontheroad.user.js
+++ ontheroad.user.js
@@ -8,10 +8,14 @@
 // @downloadURL    https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
 // @installURL     https://github.com/Hurqalia/ontheroad/raw/master/ontheroad.user.js
 // @description    [ontheroad-2016-06-12-001] OnTheRoad
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
